b4704cd16e13db2bd59c13f74d67b34c85fac5a4
phanhau1410 <phanhaunguyen1410@gmail.com>
2023-07-29 23:52:18 +0700

    Add DTOs, GlobalUsing and AutoMapper

---
+++ GlobalUsing.cs
@@ -0,0 +1,4 @@
+global using dotnet_rpg.Models;
+global using dotnet_rpg.Services.CharacterService;
+global using dotnet_rpg.DTOs.Character;
+global using AutoMapper;
--- Services/CharacterService/ICharacterService.cs
+++ Services/CharacterService/ICharacterService.cs
@@ -7,9 +7,9 @@ namespace dotnet_rpg.Services.CharacterService
 {
     public interface ICharacterService
     {
-        Task<ServiceResponse<List<Character>>> GetAllCharacter();
-        Task<ServiceResponse<Character>> GetCharacterById(int id);
-        Task<ServiceResponse<List<Character>>> AddCharacter(Character newCharacter);
+        Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacter();
+        Task<ServiceResponse<GetCharacterDto>> GetCharacterById(int id);
+        Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto newCharacter);
 
     }
 }
