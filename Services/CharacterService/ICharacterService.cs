63468f174325934156010076fa27e7c9275552bd
phanhau1410 <phanhaunguyen1410@gmail.com>
2023-07-30 21:56:56 +0700

    Add HttpPut method to update character

--- Services/CharacterService/ICharacterService.cs
+++ Services/CharacterService/ICharacterService.cs
@@ -10,6 +10,7 @@ namespace dotnet_rpg.Services.CharacterService
         Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacter();
         Task<ServiceResponse<GetCharacterDto>> GetCharacterById(int id);
         Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto newCharacter);
+        Task<ServiceResponse<GetCharacterDto>> UpdateCharacter(UpdateCharacterDto updatedCharacter);
 
     }
 }
