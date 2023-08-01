19a7b7e8b7125713869c8f679c34eb6ae07dacc9
phanhau1410 <phanhaunguyen1410@gmail.com>
2023-08-01 12:32:07 +0700

    Using EF to connect with MSSQL

--- GlobalUsing.cs
+++ GlobalUsing.cs
@@ -2,3 +2,5 @@ global using dotnet_rpg.Models;
 global using dotnet_rpg.Services.CharacterService;
 global using dotnet_rpg.DTOs.Character;
 global using AutoMapper;
+global using Microsoft.EntityFrameworkCore;
+global using dotnet_rpg.Data;
